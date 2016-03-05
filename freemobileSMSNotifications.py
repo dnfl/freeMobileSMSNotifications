3a51ceda144054399f0a08e0ac59597f891f2473
Anmi <dnfl@users.noreply.github.com>
2016-03-05 15:51:04 +0100

    Script works

--- freemobileSMSNotifications.py
+++ freemobileSMSNotifications.py
@@ -1,36 +1,47 @@
 import requests
 import sys
 
-
-
 print sys.argv[1:]
-lenargv = len(sys.argv[1:])
-if lenargv != 3:
-	print('3 arguments needed')
 
-userLogin = ''
-userKey = ''
-message = ''
+def main():
 
-baseUrl = 'https://smsapi.free-mobile.fr/sendmsg?'
+    lenargv = len(sys.argv[1:])
+    if lenargv != 3:
+        print('3 arguments needed: userLogin, userKey, message')
+        return -1
+    else :
+        userLogin = sys.argv[1]
+        userKey = sys.argv[2]
+        message = sys.argv[3]
 
-payload = 'user=' + userLogin + '&pass=' + userKey + '&msg=' + message
+        baseUrl = 'https://smsapi.free-mobile.fr/sendmsg?'
 
-finalUrl = baseUrl + payload
+        payload = 'user=' + userLogin + '&pass=' + userKey + '&msg=' + message
+
+        finalUrl = baseUrl + payload
 
 
-r = requests.post(finalUrl)
+        r = requests.post(finalUrl)
 
-if r.status_code == 200:
-	print("Message sent successfully")
-elif r.status_code == 400:
-	print("One argument is missing")
-elif r.status_code == 402:
-	print("Too many sms sent, please wait and retry")
-elif r.status_code == 403:
-	print("The service is not activated on the line holder's account, or incorrect login/userkey")
-elif r.status_code == 500:
-	print("Server side error. Please try again later")
+        if r.status_code == 200:
+                print("Message sent successfully")
+                print("Stopping execution")
+                return 0
+        elif r.status_code == 400:
+                print("One argument is missing")
+                print("Stopping execution")
+                return 400
+        elif r.status_code == 402:
+                print("Too many sms sent, please wait and retry")
+                print("Stopping execution")
+                return 402
+        elif r.status_code == 403:
+                print("The service is not activated on the line holder's account, or incorrect login/userkey")
+                print("Stopping execution")
+                return 403
+        elif r.status_code == 500:
+                print("Server side error. Please try again later")
+                print("Stopping execution")
+                return 500
 
-print("Stopping execution")
-
+main()
